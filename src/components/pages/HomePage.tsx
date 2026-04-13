914a4a1b043d8dcfcd076837cf78a79b357437ac
Wix Remote Machine <support@wix.com>
2026-04-13 16:57:41 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -440,7 +440,7 @@ export default function HomePage() {
         {/* Login */}
         <div className="container mx-auto px-4 max-w-4xl">
           <AnimatedElement>
-            <SectionBanner>How To Log In to Tiranga Game?</SectionBanner>
+            <SectionBanner>How To Log In to Dostwin?</SectionBanner>
           </AnimatedElement>
           <div className="flex flex-col md:flex-row gap-12 items-center">
             <AnimatedElement delay={100} className="flex-1 space-y-6">
