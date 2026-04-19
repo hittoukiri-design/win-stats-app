dc446c123ac6e88aebf41cda72225c2fbf459c35
Wix Remote Machine <support@wix.com>
2026-04-19 11:07:49 +0000

    Published

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -564,11 +564,13 @@ export default function HomePage() {
               </ul>
             </AnimatedElement>
             <AnimatedElement delay={200} className="flex-1 flex justify-center gap-4">
-               <div className="relative w-48 h-[380px] bg-zinc-900 rounded-[2rem] border-4 border-zinc-800 overflow-hidden shadow-xl hidden md:block">
-                <Image src="https://dostwin.app/uploads/dostwin-gift-code.webp" alt="Promotion" className="w-full h-full object-cover" />
-              </div>
+
               <div className="relative w-56 h-[450px] bg-zinc-900 rounded-[2.5rem] border-8 border-zinc-800 overflow-hidden shadow-2xl z-10">
-                <Image src="https://dostwin.app/uploads/dostwin-referral.webp" alt="Invite Link" className="w-full h-full object-cover" />
+                <Image
+                  src="https://static.wixstatic.com/media/dc7695_66f84457509344bf9559e9a3fd2217a5~mv2.png"
+                  className="w-full h-full object-cover"
+                  originWidth={597}
+                  originHeight={1148} />
               </div>
             </AnimatedElement>
           </div>
